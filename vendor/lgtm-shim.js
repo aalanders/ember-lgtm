58f9f48699a813184ffd1c7167a137a9d031fda3
Miguel Madero <mmadero@practicefusion.com>
2015-11-01 21:28:04 -0800

    Configures LGTM

--- vendor/lgtm-shim.js
+++ vendor/lgtm-shim.js
@@ -1,6 +1,8 @@
-/* globals define, LGTM */
+/* globals define, Ember, LGTM */
+
 (function () {
   define('lgtm', [], function () {
+    LGTM.configure('defer', Ember.RSVP.defer);
     return { 'default': LGTM };
   });
 })();
