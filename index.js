74a4ce7d43bab21685e381a505fc230367f9f7ea
Dahlstrom, Jesse <jdahlstrom@practicefusion.com>
2019-01-22 13:03:02 -0500

    Add lgtm to the vendor tree, then import it into the app

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 /* eslint-env node */
 var path = require('path');
-var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
+var UnwatchedDir = require('broccoli-source').UnwatchedDir;
+var funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-lgtm',
   included: function (app) {
-    app.import('vendor/lgtm.js');
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/lgtm/lgtm.js', { prepend: true });
     app.import('vendor/shims/lgtm.js');
   },
-  treeForVendor: function (vendorTree) {
-    var lgtmTree = new Funnel(new Funnel(path.dirname(require.resolve('lgtm/dist/lgtm.js'))), {
-      files: ['lgtm.js'],
-    });
-    return new MergeTrees([vendorTree, lgtmTree]);
+  treeForVendor(vendorTree) {
+    var trees = [];
+    var lgtmPath = path.dirname(require.resolve('lgtm'));
+    var lgtmNode = new UnwatchedDir(lgtmPath);
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(
+      funnel(lgtmNode, {
+        destDir: 'lgtm'
+      })
+    );
+
+    return mergeTrees(trees);
   }
 };
