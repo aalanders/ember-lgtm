77d3b5eba7a7abbdce9ca88f214315830649e977
Dahlstrom, Jesse <jdahlstrom@practicefusion.com>
2019-01-22 13:02:28 -0500

    Remove unnecessary blueprint

--- blueprints/ember-lgtm/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  normalizeEntityName: function() {},
-  description: 'Default blueprint for ember-lgtm, installs LGTM in package.json',
-  afterInstall: function () {
-    return this.addPackageToProject('lgtm');
-  }
-};
