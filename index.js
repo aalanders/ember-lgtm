1bb48078a77bb99aac57403aa3688079d8b6a836
Dahlstrom, Jesse <jdahlstrom@practicefusion.com>
2019-01-22 13:33:34 -0500

    Fix lint error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 var path = require('path');
-var UnwatchedDir = require('broccoli-source').UnwatchedDir;
 var funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
 
@@ -15,14 +14,13 @@ module.exports = {
   treeForVendor(vendorTree) {
     var trees = [];
     var lgtmPath = path.dirname(require.resolve('lgtm'));
-    var lgtmNode = new UnwatchedDir(lgtmPath);
 
     if (vendorTree) {
       trees.push(vendorTree);
     }
 
     trees.push(
-      funnel(lgtmNode, {
+      funnel(lgtmPath, {
         destDir: 'lgtm'
       })
     );
